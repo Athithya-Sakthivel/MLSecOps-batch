6a545e1093e84f6daafc20e86b1464044ead481c
Athithya-Sakthivel <athithya651@gmail.com>
2026-04-06 01:31:17 +0000

    elt security fix

--- src/workflows/train/commands.sh
+++ src/workflows/train/commands.sh
@@ -1,6 +1,6 @@
 # ALWAYS: no spaces around '='
 export S3_BUCKET="e2e-mlops-data-681802563986"
-export TRAIN_TASK_IMAGE="ghcr.io/athithya-sakthivel/flyte-train-task:2026-04-03-08-16--475813f@sha256:3182ade927934a1f5efc98e4df457fccb76521b442fc1133e192bb03d632adba"
+export TRAIN_TASK_IMAGE="ghcr.io/athithya-sakthivel/flyte-train-task:2026-04-06-06-22--6610d5f@sha256:0dd8cb8701a653cfc7cc103d30f4437ec6e625d41cd57f5bc6c45223512e7acd"
 export TRAIN_PROFILE="${TRAIN_PROFILE:-staging}"
 export PYTHONPATH="$PWD/src${PYTHONPATH:+:$PYTHONPATH}"
 
